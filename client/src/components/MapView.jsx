e3786771b6095751d51b3eafda8120d6c603ccc8
Parthav <files0792@gmail.com>
2026-04-12 17:52:06 +0530

    fix image error

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -1,30 +1,33 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-const customIcon = new L.Icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-});
-
-console.log("ICON:", customIcon);
 export default function MapView({ issues }) {
+  const customIcon = L.icon({
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    iconRetinaUrl:
+      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    shadowUrl:
+      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+
   return (
     <MapContainer
-      center={[22.9734, 78.6569]} // India center
+      center={[22.9734, 78.6569]}
       zoom={5}
       style={{ height: "500px", width: "100%" }}
     >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
       {issues.map((issue) =>
         issue.coordinates ? (
-          <Marker key={issue.id} position={issue.coordinates} icon={customIcon}>
+          <Marker
+            key={issue.id}
+            position={issue.coordinates}
+            icon={customIcon}
+          >
             <Popup>
               <strong>{issue.title}</strong>
               <br />
